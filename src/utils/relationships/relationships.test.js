cf89fd8432786f93971d1042100d0f9ba5ebdfcd
Thomas Chia <thomas.chia@amaas.com>
2017-07-21 19:14:38 +0800

    Fix utils/relationships tests. AMAAS-950

--- src/utils/relationships/relationships.test.js
+++ src/utils/relationships/relationships.test.js
@@ -2,69 +2,71 @@ import uuid from 'uuid'
 import { retrieve, insert, amend } from './relationships'
 import Relationship from '../../relationships'
 import * as api from '../../exports/api'
+import * as network from '../network'
+
+network.retrieveData = jest.fn()
+network.insertData = jest.fn()
+network.patchData = jest.fn()
+network.putData = jest.fn()
+network.searchData = jest.fn()
+network.deleteData = jest.fn()
 
 api.config({
-  stage: 'staging',
-  token: process.env.API_TOKEN
+  stage: 'staging'
 })
-// jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000
 
-const AMId = 1
+const mockRel = {
+  assetManagerId: 1,
+  relationshipId: 'ID',
+  relatedId: 2,
+  relationshipType: 'Employee',
+  clientId: 5,
+  relationshipStatus: 'Pending',
+}
 
-describe('utils/relationships', () => {
-  describe('retrieve', () => {
-    it('retrieves', done => {
-      retrieve({ AMId })
-        .then(res => {
-          expect(res).toBeDefined()
-          done()
-        })
-        .catch(err => {
-          console.error(err)
-        })
+describe('retrieve', () => {
+  beforeAll(() => {
+    network.retrieveData.mockImplementation(() => Promise.resolve(mockRel))
+  })
+  test('with promise', () => {
+    let promise = retrieve({})
+    expect(promise).toBeInstanceOf(Promise)
+  })
+  it('calls retrieveData with correct params', done => {
+    retrieve({ AMId: 1, resourceId: 'testID' }, (error, result) => {
+      expect(network.retrieveData).toHaveBeenCalledWith({ AMaaSClass: 'relationships', AMId: 1 })
+      done()
     })
   })
+})
 
-  describe('insert', () => {
-    it.skip('inserts', done => {
-      const rel = {
-        assetManagerId: AMId,
-        relationshipType: "External",
-        relatedId: 10,
-        relationshipId: uuid().substring(0, 10)
-      }
-      insert({ AMId: rel.assetManagerId, relationship: rel })
-        .then(res => {
-          expect(res).toEqual(expect.objectContaining(rel))
-          done()
-        })
-        .catch(err => {
-          console.error(err)
-        })
+describe('insert', () => {
+  beforeAll(() => {
+    network.insertData.mockImplementation(() => Promise.resolve(mockRel))
+  })
+  test('with promise', () => {
+    let promise = insert({})
+    expect(promise).toBeInstanceOf(Promise)
+  })
+  it('calls insertData with correct params', done => {
+    insert({ AMId: 1, relationship: mockRel }, (error, result) => {
+      expect(network.insertData).toHaveBeenCalledWith({ AMaaSClass: 'relationships', AMId: 1, data: JSON.parse(JSON.stringify(mockRel)) })
+      done()
     })
   })
+})
 
-  describe('amend', () => {
-    it('amends', async done => {
-      let type
-      let res = await retrieve({ AMId })
-      if (res.length === 0) {
-        console.error('amend: Result is empty, force fail on timeout.')
-        return
-      }
-      res = res.filter(rel => rel.assetManagerId !== 0 && rel.relationshipStatus !== 'Inactive')
-      if (res.length === 0) {
-        console.error('amend: Result is empty, force fail on timeout.')
-        return
-      }
-      res = res[0]
-      if (res.relationshipType === 'External') {
-        res.relationshipType = 'Employee'
-      } else {
-        res.relationshipType = 'External'
-      }
-      res = await amend({ AMId: res.assetManagerId, relationship: res })
-      expect(res.relationshipType).toEqual(expect.stringMatching(/(Employee|External)/))
+describe('amend', () => {
+  beforeAll(() => {
+    network.putData.mockImplementation(() => Promise.resolve(mockRel))
+  })
+  test('with promise', () => {
+    let promise = amend({})
+    expect(promise).toBeInstanceOf(Promise)
+  })
+  it('calls putData with correct params', done => {
+    amend({ AMId: 1, relationship: mockRel }, (error, result) => {
+      expect(network.putData).toHaveBeenCalledWith({ AMaaSClass: 'relationships', AMId: 1, data: JSON.parse(JSON.stringify(mockRel)) })
       done()
     })
   })
