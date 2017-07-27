5a463d8b865181667b7507f264d01f1877ae1457
LJY123 <li474412210@gmail.com>
2017-07-27 12:09:20 +0800

    Add comments

--- src/utils/fundamentals/fundamentals.js
+++ src/utils/fundamentals/fundamentals.js
@@ -1,14 +1,14 @@
 import * as utils from '../network/utils'
 import { searchData } from '../network'
+
 /**
  * Make request and search data
  * @function countries
  * @param {object} params - object of parameters:
  * @param {string} params.countryCode 
  * @param {function} [callback] - Called with two arguments (error, result) on completion. `result` is a country name returned or country names. Omit to return Promise
- * @returns {Promise|null} If no callback supplied, returns a Promise that resolves with an string
+ * @returns {Promise|null} 
  */
-
 export function countries({ code }, callback) {
   const params = {
     AMaaSClass: 'fundamentalCountries',
@@ -30,6 +30,17 @@ export function countries({ code }, callback) {
   promise.catch(error => callback(error))
 }
 
+/**
+ * Make request and search data
+ * @function calcBusinessDate
+ * @param {object} params - object of parameters:
+ * @param {string} params.codes - A country code string or an array of country codes strings 
+ * @param {string} params.date - start date
+ * @param {string} params.offset
+ * @param {string} params.invalidDates - an invalid date string or an array of invalid dates strings 
+ * @param {function} [callback] - Called with two arguments (error, result) on completion. `result` is a country name returned or country names. Omit to return Promise
+ * @returns {Promise|null} 
+ */
 export function calcBusinessDate({date, codes, offset, invalidDates}, callback) //invalid dates
 {
     if(!date){
@@ -68,20 +79,20 @@ export function calcBusinessDate({date, codes, offset, invalidDates}, callback) 
    if(!invalidDates){
           params = {
           AMaaSClass: 'fundamentalBusinessDate',
-          query: {stratDate: [date], countryCode: [code], offset: [offset]}
+          query: {startDate: [date], countryCode: [code], offset: [offset]}
           }
    }else{
          params = {
           AMaaSClass: 'fundamentalBusinessDate',
-          query: {stratDate: [date], countryCode: [code], offset: [offset], invalidDate: [invalidDate]}
+          query: {startDate: [date], countryCode: [code], offset: [offset], invalidDate: [invalidDate]}
           }
    }
 
     let promise = searchData(params).then(result => {
     if (typeof callback === 'function') {
       callback(null, result)
-    }
-      return result //should return biz date!
+    } 
+      return result 
     }).catch((err) => {
       console.log(err.message);
     });
@@ -92,6 +103,15 @@ export function calcBusinessDate({date, codes, offset, invalidDates}, callback) 
   promise.catch(error => callback(error))
 }
 
+/**
+ * Make request and search data
+ * @function processDateInfo
+ * @param {object} params - object of parameters:
+ * @param {string} params.codes - A country code string or an array of country codes strings 
+ * @param {string} params.date - start date
+ * @param {function} [callback] - Called with two arguments (error, result) on completion. `result` is a country name returned or country names. Omit to return Promise
+ * @returns {Promise|null} 
+ */
 export function processDateInfo ({date, codes}, callback)
 {
     let code=""
@@ -106,13 +126,13 @@ export function processDateInfo ({date, codes}, callback)
 
     const params = {
           AMaaSClass: 'fundamentalDateInfo',
-          query: {stratDate: [date], countryCode: [code]}
+          query: {startDate: [date], countryCode: [code]}
     }
    let promise = searchData(params).then(result => {
     if (typeof callback === 'function') {
       callback(null, result)
-    } //should retun true
-      return result //should return biz date!
+    } 
+      return result 
     }).catch((err) => {
       console.log(err.message);
     });
@@ -123,6 +143,15 @@ export function processDateInfo ({date, codes}, callback)
   promise.catch(error => callback(error))
 }
 
+/**
+ * Make request and search data
+ * @function holidays
+ * @param {object} params - object of parameters:
+ * @param {string} params.codes - A country code string or an array of country codes strings 
+ * @param {string} params.year - A year string or an array of 
+ * @param {function} [callback] - Called with two arguments (error, result) on completion. `result` is a country name returned or country names. Omit to return Promise
+ * @returns {Promise|null} 
+ */
 export function holidays ({codes, years}, callback)
 {
     if(!codes) {
@@ -157,8 +186,8 @@ export function holidays ({codes, years}, callback)
     let promise = searchData(params).then(result => {
     if (typeof callback === 'function') {
       callback(null, result)
-    } //should retun true
-      return result //should return biz date!
+    } 
+      return result 
     }).catch((err) => {
       console.log(err.message);
     });
