2af7b64d0fa34d809da1c8a5c3207b8b52ab7f77
LJY123 <li474412210@gmail.com>
2017-07-27 11:31:25 +0800

    Change method name

--- src/utils/fundamentals/fundamentals.test.js
+++ src/utils/fundamentals/fundamentals.test.js
@@ -8,16 +8,16 @@ let mockCountry = {
 }
 let mockBizDate = {
     date: '2017-6-30',
-    code: 'SG',
+    codes: 'SG',
     offset: 1
 }
 let mockDateInfo = {
     date: '2017-7-03',
-    code: 'SG'
+    codes: 'SG'
 }
 let mockHoliday = {
-    code: 'SG', 
-    year: '2017'
+    codes: 'SG,CHN', 
+    years: '2017'
 }
 
 describe('utils/fundamentals', () => {
@@ -42,28 +42,52 @@ describe('utils/fundamentals', () => {
           network.searchData.mockImplementation(() => Promise.resolve(mockBizDate))
         })
         test('with promise', () => {
-            let promise = fundamentals.calcBusinessDate({date: '2017-6-30', code: 'SG', offset: 1})
+            let promise = fundamentals.calcBusinessDate({date: '2017-6-30', codes: 'SG', offset: 1})
             expect(promise).toBeInstanceOf(Promise)
         })           
         it('calls searchData with the correct parameters', done => {
-              fundamentals.calcBusinessDate( {date: '2017-6-30', code: 'SG', offset: 1}, (error, result) => {
+              fundamentals.calcBusinessDate( {date: '2017-6-30', codes: 'SG', offset: 1}, (error, result) => {
               expect(network.searchData).toHaveBeenCalledWith({ AMaaSClass: 'fundamentalBusinessDate', query: {stratDate: ['2017-6-30'], countryCode: ['SG'], offset: [1]} })
+              done()
+            })
+        })
+        it('calls searchData with the correct parameters', done => {
+              fundamentals.calcBusinessDate( {date: '2017-6-30', codes: ['SG', 'CHN'], offset: 1}, (error, result) => {
+              expect(network.searchData).toHaveBeenCalledWith({ AMaaSClass: 'fundamentalBusinessDate', query: {stratDate: ['2017-6-30'], countryCode: ['SG,CHN'], offset: [1]} })
+              done()
+            })
+        })
+        it('calls searchData with the correct parameters', done => {
+              fundamentals.calcBusinessDate( {date: '2017-6-30', codes: 'SG', offset: 1, invalidDates: '2017-7-01'}, (error, result) => {
+              expect(network.searchData).toHaveBeenCalledWith({ AMaaSClass: 'fundamentalBusinessDate', query: {stratDate: ['2017-6-30'], countryCode: ['SG'], offset: [1], invalidDate: ['2017-7-01']} })
+              done()
+            })
+        })
+        it('calls searchData with the correct parameters', done => {
+              fundamentals.calcBusinessDate( {date: '2017-6-30', codes: 'SG', offset: 1, invalidDates: ['2017-7-01', '2017-7-02']}, (error, result) => {
+              expect(network.searchData).toHaveBeenCalledWith({ AMaaSClass: 'fundamentalBusinessDate', query: {stratDate: ['2017-6-30'], countryCode: ['SG'], offset: [1], invalidDate: ['2017-7-01,2017-7-02']} })
               done()
             })
         })
     })
 
-     describe('getDateInfo', () => {
+     describe('processDateInfo', () => {
         beforeAll(() => {
           network.searchData.mockImplementation(() => Promise.resolve(mockDateInfo))
         })
         test('with promise', () => {
-            let promise = fundamentals.getDateInfo({date: '2017-7-03', code: 'SG'})
+            let promise = fundamentals.processDateInfo({date: '2017-7-03', codes: 'SG'})
             expect(promise).toBeInstanceOf(Promise)
         })           
         it('calls searchData with the correct parameters', done => {
-              fundamentals.getDateInfo( {date: '2017-7-03', code: 'SG'}, (error, result) => {
+              fundamentals.processDateInfo( {date: '2017-7-03', codes: 'SG'}, (error, result) => {
               expect(network.searchData).toHaveBeenCalledWith({ AMaaSClass: 'fundamentalDateInfo', query: {stratDate: ['2017-7-03'], countryCode: ['SG']} })
+              done()
+            })
+        })
+        it('calls searchData with the correct parameters', done => {
+              fundamentals.processDateInfo( {date: '2017-7-03', codes: ['SG', 'CHN']}, (error, result) => {
+              expect(network.searchData).toHaveBeenCalledWith({ AMaaSClass: 'fundamentalDateInfo', query: {stratDate: ['2017-7-03'], countryCode: ['SG,CHN']} })
               done()
             })
         })
@@ -74,12 +98,24 @@ describe('utils/fundamentals', () => {
           network.searchData.mockImplementation(() => Promise.resolve(mockHoliday))
         })
         test('with promise', () => {
-            let promise = fundamentals.holidays({code: 'SG', year: 2017})
+            let promise = fundamentals.holidays({codes: 'SG', years: 2017})
             expect(promise).toBeInstanceOf(Promise)
+        })
+        it('calls searchData with the correct parameters', done => {
+              fundamentals.holidays( {codes: ['SG'], years: 2017}, (error, result) => {
+              expect(network.searchData).toHaveBeenCalledWith({ AMaaSClass: 'fundamentalHoliday', query: {countryCode: ['SG'], year: [2017]} })
+              done()
+            })
         })           
         it('calls searchData with the correct parameters', done => {
-              fundamentals.holidays( {code: 'SG', year: 2017}, (error, result) => {
-              expect(network.searchData).toHaveBeenCalledWith({ AMaaSClass: 'fundamentalHoliday', query: {countryCode: ['SG'], year: [2017]} })
+              fundamentals.holidays( {codes: ['SG','CHN'], years: 2017}, (error, result) => {
+              expect(network.searchData).toHaveBeenCalledWith({ AMaaSClass: 'fundamentalHoliday', query: {countryCode: ['SG,CHN'], year: [2017]} })
+              done()
+            })
+        })       
+        it('calls searchData with the correct parameters', done => {
+              fundamentals.holidays( {codes: ['SG'], years: [2017, 2018]}, (error, result) => {
+              expect(network.searchData).toHaveBeenCalledWith({ AMaaSClass: 'fundamentalHoliday', query: {countryCode: ['SG'], year: ['2017,2018']} })
               done()
             })
         })
