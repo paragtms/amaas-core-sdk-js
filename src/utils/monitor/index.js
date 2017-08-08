b713bf04a33783232b5f7b7662c7e801ac96815e
Thomas Chia <thomas.chia@amaas.com>
2017-08-08 20:48:58 +0800

    Export the retrieveItem function for Monitor.

--- src/utils/monitor/index.js
+++ src/utils/monitor/index.js
@@ -3,6 +3,6 @@
  * @memberof module:api
  */
 
- import { insertNewItem, resubmitItem, searchItems, closeItem } from './monitor.js'
+ import { insertNewItem, resubmitItem, retrieveItem, searchItems, closeItem } from './monitor.js'
 
- export { insertNewItem, resubmitItem, searchItems, closeItem }
+ export { insertNewItem, resubmitItem, retrieveItem, searchItems, closeItem }
