7c00fe3e78215f1fcec532d7f1c16dc1e287dd4d
Thomas Chia <thomas.chia@amaas.com>
2017-09-05 18:46:24 +0800

    Update user pool to use staging pool for dev as well.

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const endpoint = {
 }
 
 const userPoolConfig = {
-  dev: { userPoolId: 'us-west-2_wKa82vECF', clientAppId: '5mrqm1sjmfp80k8foasq83rb9k' },
+  dev: { userPoolId: 'ap-southeast-1_De6j7TWIB', clientAppId: '2qk35mhjjpk165vssuqhqoi1lk' },
   staging: { userPoolId: 'ap-southeast-1_De6j7TWIB', clientAppId: '2qk35mhjjpk165vssuqhqoi1lk' },
   prod: {}
 }
