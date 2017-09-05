40c40b5a684aa2c00568f6d9d291b462aba30799
Thomas Chia <thomas.chia@amaas.com>
2017-09-05 11:21:05 +0800

    Update staging config for user pool.

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const endpoint = {
 
 const userPoolConfig = {
   dev: { userPoolId: 'us-west-2_wKa82vECF', clientAppId: '5mrqm1sjmfp80k8foasq83rb9k' },
-  staging: { userPoolId: 'ap-southeast-1_zzL5MjFxg', clientAppId: 'irjfbh2d4g5dfs1ni9blncnso' },
+  staging: { userPoolId: 'ap-southeast-1_De6j7TWIB', clientAppId: '2qk35mhjjpk165vssuqhqoi1lk' },
   prod: {}
 }
 
