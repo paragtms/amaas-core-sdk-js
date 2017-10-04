d49d73f7f42ceaf881e47a006bb1e27e95988df5
Thomas Chia <thomas.chia@amaas.com>
2017-10-04 17:25:20 +0800

    Export ForeignExchangeBase. AMAAS-1625

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,29 +9,64 @@ export { default as Derivative } from './Derivative/Derivative/derivative'
 export { default as BondOption } from './Derivative/BondOption/bondOption'
 export { default as CFD } from './Derivative/CFD/cfd'
 export { default as Equity } from './Equity/equity'
-export { default as ForeignExchangeOption } from './Derivative/ForeignExchangeOption/foreignExchangeOption'
+export {
+  default as ForeignExchangeOption
+} from './Derivative/ForeignExchangeOption/foreignExchangeOption'
 export { default as Fund } from './Fund/Fund/fund'
 export { default as ExchangeTradedFund } from './Fund/ExchangeTradedFund/etf'
-export { default as ForeignExchange } from './FX/ForeignExchange/foreignExchange'
-export { default as ForeignExchangeForward } from './FX/ForeignExchangeForward/foreignExchangeForward'
+export {
+  default as ForeignExchange
+} from './FX/ForeignExchange/foreignExchange'
+export {
+  default as ForeignExchangeSpot
+} from './FX/ForeignExchangeSpot/foreignExchangeSpot'
+export {
+  default as ForeignExchangeForward
+} from './FX/ForeignExchangeForward/foreignExchangeForward'
 export { default as Index } from './IndexAsset/IndexAsset/indexAsset'
 export { default as BondFuture } from './ListedDerivative/BondFuture/bondFuture'
-export { default as CommodityFuture } from './ListedDerivative/CommodityFuture/commodityFuture'
-export { default as BondFutureOption } from './ListedDerivative/BondFutureOption/bondFutureOption'
-export { default as EnergyFuture } from './ListedDerivative/EnergyFuture/energyFuture'
-export { default as EquityFuture } from './ListedDerivative/EquityFuture/equityFuture'
-export { default as ForeignExchangeFuture } from './ListedDerivative/ForeignExchangeFuture/foreignExchangeFuture'
+export {
+  default as CommodityFuture
+} from './ListedDerivative/CommodityFuture/commodityFuture'
+export {
+  default as BondFutureOption
+} from './ListedDerivative/BondFutureOption/bondFutureOption'
+export {
+  default as EnergyFuture
+} from './ListedDerivative/EnergyFuture/energyFuture'
+export {
+  default as EquityFuture
+} from './ListedDerivative/EquityFuture/equityFuture'
+export {
+  default as ForeignExchangeFuture
+} from './ListedDerivative/ForeignExchangeFuture/foreignExchangeFuture'
 export { default as Future } from './ListedDerivative/Future/future'
-export { default as FutureOption } from './ListedDerivative/FutureOption/futureOption'
-export { default as IndexFuture } from './ListedDerivative/IndexFuture/indexFuture'
-export { default as InterestRateFuture } from './ListedDerivative/InterestRateFuture/interestRateFuture'
-export { default as ListedContractForDifference } from './ListedDerivative/ListedCFD/listedCFD'
-export { default as ListedDerivative } from './ListedDerivative/ListedDerivative/listedDerivative'
-export { default as PrivateInvestment } from './PrivateInvestment/privateInvestment'
+export {
+  default as FutureOption
+} from './ListedDerivative/FutureOption/futureOption'
+export {
+  default as IndexFuture
+} from './ListedDerivative/IndexFuture/indexFuture'
+export {
+  default as InterestRateFuture
+} from './ListedDerivative/InterestRateFuture/interestRateFuture'
+export {
+  default as ListedContractForDifference
+} from './ListedDerivative/ListedCFD/listedCFD'
+export {
+  default as ListedDerivative
+} from './ListedDerivative/ListedDerivative/listedDerivative'
+export {
+  default as PrivateInvestment
+} from './PrivateInvestment/privateInvestment'
 export { default as RealAsset } from './RealAsset/RealAsset/realAsset'
 export { default as RealEstate } from './RealAsset/RealEstate/realEstate'
 export { default as Wine } from './RealAsset/Wine/wine'
 export { default as Sukuk } from './Sukuk/Sukuk/sukuk'
 export { default as Synthetic } from './Synthetic/Synthetic/synthetic'
-export { default as SyntheticFromBook } from './Synthetic/SyntheticFromBook/syntheticFromBook'
-export { default as SyntheticMultiLeg } from './Synthetic/SyntheticMultiLeg/syntheticMultiLeg'
+export {
+  default as SyntheticFromBook
+} from './Synthetic/SyntheticFromBook/syntheticFromBook'
+export {
+  default as SyntheticMultiLeg
+} from './Synthetic/SyntheticMultiLeg/syntheticMultiLeg'
