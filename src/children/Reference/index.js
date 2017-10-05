4f8f132fd9e23ecdba3f5b44a4a8ceded8f641fc
Thomas Chia <thomas.chia@amaas.com>
2017-10-05 12:21:20 +0800

    Update docs. AMAAS-1648

--- src/children/Reference/index.js
+++ src/children/Reference/index.js
@@ -10,6 +10,7 @@ class Reference extends AMaaSModel {
     * Construct a new Reference instance
     * @param {object} params - Reference creation options:
     * @param {string} params.referenceValue - The identifier of this Reference (e.g. transactionId)
+    * @param {boolean} [params.referencePrimary=false] - Whether the Reference is primary (a parent can only have 1 primary Reference)
     * @param {string} [params.createdBy] - ID of the user that created the Reference
     * @param {string} [params.updatedBy] - ID of the user that updated the Reference
     * @param {date} [params.createdTime] - Time that the Reference was created
